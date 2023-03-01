3516bf663ec8c29fdb3929789783758344811445
Yacine Haddouche <100038941+gityacine@users.noreply.github.com>
2023-03-01 19:16:34 +0100

    changed the link to assets

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Card from "./components/Card"
 function App() {
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-[#e0e8ff] bg-[url('pattern-background-mobile.svg')] sm:bg-[url('pattern-background-desktop.svg')] bg-contain bg-no-repeat">
+    <div className="flex items-center justify-center min-h-screen bg-[#e0e8ff] bg-[url('/assets/pattern-background-mobile.svg')] sm:bg-[url('/assets/pattern-background-desktop.svg')] bg-contain bg-no-repeat">
       <Card />
     </div>
   )
